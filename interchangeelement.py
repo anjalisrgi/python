61ea02da551b2a4ab69e7c4c89b91318b35e44ce
anjalisrgi <161213844+anjalisrgi@users.noreply.github.com>
2024-03-06 17:46:16 +0530

    Update interchangeelement.py

--- interchangeelement.py
+++ interchangeelement.py
@@ -1,11 +1,9 @@
 # Python program to swap first and last element of a list
-
-
 def swapList(newList):
 	
 	newList[0], newList[-1] = newList[-1], newList[0]
 
 	return newList
 #code	
-newList = [12, 35, 9, 56, 24]
+newList = [36, 34, 32, 30, 29]
 print(swapList(newList))
