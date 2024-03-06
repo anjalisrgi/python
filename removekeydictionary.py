34207bafef445062e130000a86507409dcfaed4e
anjalisrgi <161213844+anjalisrgi@users.noreply.github.com>
2024-03-06 17:54:38 +0530

    Create removekeydictionary.py

---
+++ removekeydictionary.py
@@ -0,0 +1,5 @@
+test_dict = {"Anjali": 18, "Ankita": 19, "Ananya": 20}
+print("The dictionary before performing remove is : ", test_dict)
+del test_dict['Ankita']
+print("The dictionary after remove is : ", test_dict)
+del test_dict['Ankita']
